6c5872b33552562df7a1df3c188e9cf47900dcf0
luckpp <marius.andritoi@yahoo.com>
2020-07-20 10:33:54 +0300

    started to use React state on nav menu

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -2,13 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export default class Popular extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLanguage: 'All'
+    }
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(selectedLanguage) {
+    this.setState({
+      selectedLanguage
+    })
+  }
+
   render() {
     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Pythpn'];
     return (
       <ul className='flex-center'>
         {languages.map((language) => (
           <li key={language}>
-            <button className='btn-clear nav-link'>
+            <button
+              onClick={() => this.updateLanguage(language)}
+              style={language === this.state.selectedLanguage ? { color: 'rgb(187, 46, 31)' } : null}
+              className='btn-clear nav-link'>
               {language}
             </button>
           </li>
