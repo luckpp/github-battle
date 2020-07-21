67610058c028568badd999d22e2da33ec04ea448
luckpp <marius.andritoi@yahoo.com>
2020-07-21 21:37:42 +0300

    result cards

---
+++ app/components/Results.js
@@ -0,0 +1,142 @@
+import React from 'react';
+import { battle } from '../utils/api';
+import { FaCompass, FaBriefcase, FaUsers, FaUserFriends, FaCode, FaUser } from 'react-icons/fa'
+
+export default class Results extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      winner: null,
+      looser: null,
+      error: null,
+      loading: true
+    }
+  }
+
+  componentDidMount() {
+    const { playerOne, playerTwo } = this.props;
+    battle([playerOne, playerTwo])
+      .then((players) => {
+        this.setState({
+          winner: players[0],
+          looser: players[1],
+          error: null,
+          loading: false
+        })
+      }).catch(({ message }) => {
+        this.setState({
+          error: message,
+          loading: false
+        })
+      });
+  }
+
+  render() {
+    const { winner, looser, error, loading } = this.state;
+
+    if (loading === true) {
+      return (
+        <p>LOADING</p>
+      )
+    }
+
+    if (error) {
+      return (
+        <p className='center-text error'>{error}</p>
+      )
+    }
+    return (
+      <div className='grid space-around container-sm'>
+        <div className='card bg-light'>
+          <h4 className='header-lg center-text'>
+            {winner.score === looser.score ? 'Tie' : 'Winner'}
+          </h4>
+          <img
+            className='avatar'
+            src={winner.profile.avatar_url}
+            alt={`Avatar for ${winner.profile.login}`}
+          />
+          <h4 className='center-text'>
+            Score: {winner.score.toLocaleString()}
+          </h4>
+          <h2 className='center-text'>
+            <a className='link' href={winner.profile.html_url}>
+              {winner.profile.login}
+            </a>
+          </h2>
+          <ul className='card-list'>
+            <li>
+              <FaUser color='rgb(239, 115, 115)' size={22} />
+              {winner.profile.name}
+            </li>
+            {winner.profile.location && (
+              <li>
+                <FaCompass color='rgb(144, 115, 255)' size={22} />
+                {winner.profile.location}
+              </li>
+            )}
+            {winner.profile.company && (
+              <li>
+                <FaBriefcase color='#795548' size={22} />
+                {winner.profile.company}
+              </li>
+            )}
+            <li>
+              <FaUsers color='rgb(129, 195, 245)' size={22} />
+              {winner.profile.followers.toLocaleString()} followers
+            </li>
+            <li>
+              <FaUserFriends color='rgb(64, 183, 95)' size={22} />
+              {winner.profile.following.toLocaleString()} following
+            </li>
+          </ul>
+        </div>
+        <div className='card bg-light'>
+          <h4 className='header-lg center-text'>
+            {winner.score === looser.score ? 'Tie' : 'Looser'}
+          </h4>
+          <img
+            className='avatar'
+            src={looser.profile.avatar_url}
+            alt={`Avatar for ${looser.profile.login}`}
+          />
+          <h4 className='center-text'>
+            Score: {looser.score.toLocaleString()}
+          </h4>
+          <h2 className='center-text'>
+            <a className='link' href={looser.profile.html_url}>
+              {looser.profile.login}
+            </a>
+          </h2>
+          <ul className='card-list'>
+            <li>
+              <FaUser color='rgb(239, 115, 115)' size={22} />
+              {looser.profile.name}
+            </li>
+            {looser.profile.location && (
+              <li>
+                <FaCompass color='rgb(144, 115, 255)' size={22} />
+                {looser.profile.location}
+              </li>
+            )}
+            {looser.profile.company && (
+              <li>
+                <FaBriefcase color='#795548' size={22} />
+                {looser.profile.company}
+              </li>
+            )}
+            <li>
+              <FaUsers color='rgb(129, 195, 245)' size={22} />
+              {looser.profile.followers.toLocaleString()} followers
+            </li>
+            <li>
+              <FaUserFriends color='rgb(64, 183, 95)' size={22} />
+              {looser.profile.following.toLocaleString()} following
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
